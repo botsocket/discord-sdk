1cce06517ce4f3bd78f90b699f01b928f6565b7c
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2021-03-27 11:06:43 +0100

    Add eslint and fix validation

--- Client.js
+++ Client.js
@@ -1,136 +1,153 @@
-"use strict";
+'use strict';
 
-const Core = require("./Core");
-const Jade = require("@botsocket/jade");
+const Core = require('./Core');
+
 module.exports = class Client {
-  constructor(parent, name = "default", payload) {
-    this._core = parent ? parent._core : new Core(payload);
-    this.plugins = this._core.plugins;
+    constructor(parent, name = 'default', payload) {
 
-    this._realm = {
-      name,
-      parent,
-    };
+        this._core = parent ? parent._core : new Core(payload);
+        this.plugins = this._core.plugins;
 
-    this.logger = this._core.logger;
-  }
+        this._realm = {
+            name,
+            parent,
+        };
 
-  clone(name) {
-    return new Client(this, name);
-  }
-
-  start(token) {
-    if (this._core.client.ready) {
-      return this;
+        this.logger = this._core.logger;
     }
 
-    return new Promise((resolve, reject) => {
-      this.event({
-        name: "ready",
-        handler: () => {
-          this.logger.info("Ready!");
-          resolve(this);
-        },
-      });
+    clone(name) {
 
-      this.event({
-        name: "message",
-        handler: (_client, message) => {
-          const matches = this._core.registry.match(message.content);
-          if (!matches) {
-            return;
-          }
-
-          for (const match of matches) {
-            const { validation, failAction, handler } = match.definition.data;
-
-            if (Jade.isSchema(validation?.args)) {
-              const result = validation.args.validate(match.args);
-              if (result.errors) {
-                if (!failAction || failAction === "error") {
-                  return message.channel.send(result.errors[0].message);
-                } else if (failAction === "ignore") {
-                  return;
-                } else if (typeof failAction === "function") {
-                  return failAction(message, result, "args");
-                }
-              }
-            }
-
-            if (Jade.isSchema(validation?.flags)) {
-              const result = validation.flags.validate(match.flags);
-              if (result.errors) {
-                if (!failAction || failAction === "error") {
-                  return message.channel.send(result.errors[0].message);
-                } else if (failAction === "ignore") {
-                  return;
-                } else if (typeof failAction === "function") {
-                  return failAction(message, result, "flags");
-                }
-              }
-            }
-
-            handler(message, match);
-          }
-        },
-      });
-
-      this.event({
-        name: "log",
-        handler: (_client, log) => {
-          console.log(
-            `[${log.type[0].toUpperCase()}${log.type.slice(1)}] ${log.message}`
-          );
-        },
-      });
-
-      setTimeout(() => {
-        reject(new Error("Timeout"));
-      }, 10000);
-
-      this._core.client.login(token);
-    });
-  }
-
-  command(...definitions) {
-    return this._core.registry.add(...definitions);
-  }
-
-  event(...events) {
-    for (const event of events) {
-      this._core.client[event.once ? "once" : "on"](event.name, (...args) =>
-        event.handler(this._core.client, ...args)
-      );
+        return new Client(this, name);
     }
-  }
 
-  async register(...plugins) {
-    for (const plugin of plugins) {
-      try {
-        if (typeof plugin.register === "function") {
-          const client = this.clone(plugin.name);
-          await plugin.register(client);
-          this.logger.info(`Plugin ${plugin.name} loaded`);
-        } else if (plugin.plugin) {
-          const client = this.clone(plugin.plugin.name);
-          await plugin.plugin.register(client, plugin.options);
-          this.logger.info(`Plugin ${plugin.plugin.name} loaded`);
+    start(token) {
+
+        if (this._core.client.ready) {
+            return this;
         }
-      } catch (e) {
-        this.logger.error(`Error while loading plugin ${plugin.name}`);
-        this.logger.error(e);
-      }
+
+        return new Promise((resolve, reject) => {
+
+            this.event({
+                name: 'ready',
+                handler: () => {
+
+                    this.logger.info('Ready!');
+                    resolve(this);
+                },
+            });
+
+            this.event({
+                name: 'message',
+                handler: (_client, message) => {
+
+                    const matches = this._core.registry.match(message.content);
+                    if (!matches) {
+                        return;
+                    }
+
+                    for (const match of matches) {
+                        const { validation, handler } = match.definition.data;
+
+                        if (validation?.args?._definition) {
+                            const result = validation.args.validate(match.args);
+                            if (result.errors) {
+                                if (!validation.failAction || validation.failAction === 'error') {
+                                    return message.channel.send(result.errors[0].message);
+                                }
+                                else if (validation.failAction === 'ignore') {
+                                    return;
+                                }
+                                else if (typeof failAction === 'function') {
+                                    return validation.failAction(message, result, 'args');
+                                }
+                            }
+                        }
+
+                        if (validation?.flags?._definition) {
+                            const result = validation.flags.validate(match.flags);
+                            if (result.errors) {
+                                if (!validation.failAction || validation.failAction === 'error') {
+                                    return message.channel.send(result.errors[0].message);
+                                }
+                                else if (validation.failAction === 'ignore') {
+                                    return;
+                                }
+                                else if (typeof failAction === 'function') {
+                                    return validation.failAction(message, result, 'flags');
+                                }
+                            }
+                        }
+
+                        handler(message, match);
+                    }
+                },
+            });
+
+            this.event({
+                name: 'log',
+                handler: (_client, log) => {
+
+                    console.log(
+                        `[${log.type[0].toUpperCase()}${log.type.slice(1)}] ${log.message}`,
+                    );
+                },
+            });
+
+            setTimeout(() => {
+
+                reject(new Error('Timeout'));
+            }, 10000);
+
+            this._core.client.login(token);
+        });
     }
-  }
 
-  expose(property, value) {
-    let exposures = this._core.plugins[this._realm.name];
+    command(...definitions) {
 
-    if (!exposures) {
-      exposures = this._core.plugins[this._realm.name] = {};
+        return this._core.registry.add(...definitions);
     }
 
-    exposures[property] = value;
-    return value;
-  }
+    event(...events) {
+
+        for (const event of events) {
+            this._core.client[event.once ? 'once' : 'on'](event.name, (...args) => event.handler(this._core.client, ...args),
+            );
+        }
+    }
+
+    async register(...plugins) {
+
+        for (const plugin of plugins) {
+            try {
+                if (typeof plugin.register === 'function') {
+                    const client = this.clone(plugin.name);
+                    await plugin.register(client);
+                    this.logger.info(`Plugin ${plugin.name} loaded`);
+                }
+                else if (plugin.plugin) {
+                    const client = this.clone(plugin.plugin.name);
+                    await plugin.plugin.register(client, plugin.options);
+                    this.logger.info(`Plugin ${plugin.plugin.name} loaded`);
+                }
+            }
+            catch (e) {
+                this.logger.error(`Error while loading plugin ${plugin.name}`);
+                this.logger.error(e);
+            }
+        }
+    }
+
+    expose(property, value) {
+
+        let exposures = this._core.plugins[this._realm.name];
+
+        if (!exposures) {
+            exposures = this._core.plugins[this._realm.name] = {};
+        }
+
+        exposures[property] = value;
+        return value;
+    }
 };
--- Core.js
+++ Core.js
@@ -1,53 +1,60 @@
-"use strict";
+'use strict';
 
-const Discord = require("discord.js");
-const Ruby = require("@botsocket/ruby");
-const { inspect } = require("util");
+const Discord = require('discord.js');
+const Ruby = require('@botsocket/ruby');
+const { inspect } = require('util');
 
 module.exports = class Core {
-  constructor(payload) {
-    this._payload = payload;
-    this.client = new Discord.Client(payload.client);
-    this.registry = Ruby.registry(payload.registry);
-    this.client.token = payload.token;
+    constructor(payload) {
 
-    this.plugins = {};
+        this._payload = payload;
+        this.client = new Discord.Client(payload.client);
+        this.registry = Ruby.registry(payload.registry);
+        this.client.token = payload.token;
 
-    this.logger = {
-      inspect: (data) => {
-        if (typeof data === "string") {
-          return data;
-        }
+        this.plugins = {};
 
-        return inspect(data);
-      },
-      join: (data) => {
-        return data.map(this.logger.inspect).join(" ");
-      },
-      info: (...data) => {
-        this.client.emit("log", {
-          type: "info",
-          message: this.logger.join(data),
-        });
-      },
-      log: (...data) => {
-        this.client.emit("log", {
-          type: "info",
-          message: this.logger.join(data),
-        });
-      },
-      warn: (...data) => {
-        this.client.emit("log", {
-          type: "warn",
-          message: this.logger.join(data),
-        });
-      },
-      error: (...data) => {
-        this.client.emit("log", {
-          type: "error",
-          message: this.logger.join(data),
-        });
-      },
-    };
-  }
+        this.logger = {
+            inspect: (data) => {
+
+                if (typeof data === 'string') {
+                    return data;
+                }
+
+                return inspect(data);
+            },
+            join: (data) => {
+
+                return data.map(this.logger.inspect).join(' ');
+            },
+            info: (...data) => {
+
+                this.client.emit('log', {
+                    type: 'info',
+                    message: this.logger.join(data),
+                });
+            },
+            log: (...data) => {
+
+                this.client.emit('log', {
+                    type: 'info',
+                    message: this.logger.join(data),
+                });
+            },
+            warn: (...data) => {
+
+                this.client.emit('log', {
+                    type: 'warn',
+                    message: this.logger.join(data),
+                });
+            },
+            error: (...data) => {
+
+                this.client.emit('log', {
+                    type: 'error',
+                    message: this.logger.join(data),
+                });
+            },
+        };
+    }
 };
